0a03631dcd722e08247d1ed0cfa5774db69031d3
AmitaiB <blickstein@gmail.com>
2015-06-21 22:03:04 -0400

    new-born project

---
+++ FISFontingAwesomeLab/FISFontingAwesomeLab/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FISFontingAwesomeLab
+//
+//  Created by Amitai Blickstein on 6/21/15.
+//  Copyright (c) 2015 Amitai's school of One. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FISFontingAwesomeLab/FISFontingAwesomeLab/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FISFontingAwesomeLab
+//
+//  Created by Amitai Blickstein on 6/21/15.
+//  Copyright (c) 2015 Amitai's school of One. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
